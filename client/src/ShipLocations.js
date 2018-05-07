34f4bb50bb39604f3fbae02cb2699bd0ac56c366
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-05-07 12:04:27 +0200

    Created grid in Frontend and showed boats on grid

--- client/src/ShipLocations.js
+++ client/src/ShipLocations.js
@@ -5,23 +5,52 @@ class ShipLocations extends React.Component{
         super();
         this.state = {
             isLoading: true,
-            locations: ""
+            gamePlayerResponse: ""
         }
     this.loadLocations = this.loadLocations.bind(this);
-
+    this.loadShipsOnGrid = this.loadShipsOnGrid.bind(this);
     }
 
     loadLocations(){
         fetch('http://localhost:8080/api/game_view/' + this.props.match.params.id, {headers: {'Access-Control-Allow-Origin':'*'}})
             .then(response => response.json())
             .then((data) => {
-                this.setState({locations: data, isLoading: false})});
+                this.setState({gamePlayerResponse: data, isLoading: false})
+                this.loadShipsOnGrid();
+            });
 
+    }
+
+    loadShipsOnGrid(){
+        this.state.gamePlayerResponse.ships.forEach((element) => {
+            element.locations.forEach((location) => {document.getElementById(location).classList.add("with-ship")})
+        })
+    }
+
+    loadGrid(){
+
+        var letterArray = ["Z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
+        var buttons;
+        function generateRow(letter, index_key){
+            buttons = letterArray.map((element, index) =>
+            { if (letter === "Z" && index !=0) {
+                    return <button key={index} className="square" id={letter + index}>{index}</button>
+                } else if (letter !== "Z" && index === 0) {
+                    return <button key={index} className="square" id={letter + index}>{letter}</button>
+                } else {
+                    return <button key={index} className="square" id={letter + index}/>
+                }})
+
+            return <div className="row" key={index_key}>{buttons}</div>
+        }
+        return letterArray.map((element, index) => generateRow(element, index))
     }
 
     componentDidMount(){
         this.loadLocations();
     }
+
+
 
     render(){
         if (this.state.isLoading) {
@@ -30,9 +59,10 @@ class ShipLocations extends React.Component{
         return (
             <div>
                 <h3>Ship Locations!</h3>
-                {this.state.locations.ships.map((ship, index) =>
+                {this.state.gamePlayerResponse.ships.map((ship, index) =>
                     <div key={index}>{ship.type}, {ship.locations}</div>
                 )}
+                <div className="grid">{this.loadGrid()}</div>
             </div>
         );
     }
