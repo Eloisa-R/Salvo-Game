dc73c18d7825fff7f0df88ff3e3d9fd6ff9035c9
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-05-08 14:40:02 +0200

    Added leaderboard in the Frontend, added bootstrap

--- client/src/Games.js
+++ client/src/Games.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
  class Games extends React.Component{
 
@@ -6,21 +8,38 @@ import React from 'react';
     super();
     this.state = {
       isLoading: true,
-      games: ""
+      games: "",
+      scores: [],
+      columns: []
     }
-    this.loadGames = this.loadGames.bind(this)
+    this.loadGames = this.loadGames.bind(this);
+    this.loadScores = this.loadScores.bind(this);
   }
 
   loadGames(){
     fetch('http://localhost:8080/api/games', {headers: {'Access-Control-Allow-Origin':'*'}})
            .then(response => response.json())
            .then((data) => {
-           this.setState({games: data, isLoading: false})});
 
+           this.setState({games: data});
+           this.loadScores();
+           });
+
+
+   }
+
+   loadScores(){
+      fetch('http://localhost:8080/api/scores', {headers: {'Access-Control-Allow-Origin':'*'}})
+          .then(response => response.json())
+          .then((data) => {
+              this.setState({scores:data, columns:[{dataField: 'email',
+                      text: 'Username'}, {dataField: 'total', text: 'Total'},{dataField: 'wins', text: 'Won'}, {dataField: 'losses', text: 'Lost'},{dataField: 'ties', text: 'Ties'}], isLoading: false})
+          })
    }
 
    componentDidMount(){
      this.loadGames();
+
    }
 
     render() {
@@ -28,13 +47,18 @@ import React from 'react';
         return <p>Loading...</p>;
       }
       return (
-        <div>
+        <div className="games-container">
         <h3>List of games</h3>
         {this.state.games.map((game, index) =>
           <div key={index}>
         Game id: {game.id}, created on: {game.created.slice(0,10)} at {game.created.slice(11,16)}, players: {game.gamePlayers.map((gp) => gp.player.email + " ")}
           </div>
         )}
+            <h3>Leaderboard</h3>
+            <div className="leaderboard">
+            <BootstrapTable keyField='email' data={ this.state.scores } columns={ this.state.columns } />
+            </div>
+
       </div>
       );
     }
