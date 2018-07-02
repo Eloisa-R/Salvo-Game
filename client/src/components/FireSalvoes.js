01406aba25ade3dbdc2eb817538671a20ed1b0e4
Eloisa-R <rios.rico.eloisa@gmail.com>
2018-07-02 17:20:58 +0200

    Updated layout of Results Component

--- client/src/components/FireSalvoes.js
+++ client/src/components/FireSalvoes.js
@@ -7,9 +7,11 @@ class FireSalvoes extends React.Component{
             <h4>Now Fire some Salvoes!</h4>
             <div>Click on the squares to fire up to 5 times, then click Submit when you're ready!</div>
             <div>{Array.from(this.props.salvoPositions).map(el => <button key={el} onClick={() => this.props.handleUndoSalvo(el)} className="undo-salvo">Undo {el}</button>)}</div>
+            <div className="salvoes-grid-and_submit">
             <Grid clickSalvo={this.props.handleClickSalvo} takenPositions={this.props.takenPositions}
                   gridType={this.props.gridType} newTakenPositions={this.props.newTakenPositions}/>
-            <button onClick={this.props.handleSubmitSalvo}>Submit</button>
+            <button className="submit-salvoes" onClick={this.props.handleSubmitSalvo}>Submit</button>
+            </div>
         </div>
     )
 
--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -5,10 +5,12 @@ class Results extends React.Component{
     render(){
         return(<div className="fire-salvoes">
             <div>
-            <Grid data={this.props.data} title={"My Ships"} gridType={"sh"}
-                  sunkenPositions={this.props.sunkenPositions} takenPositions={this.props.allShipsArray}/>
-            <Grid data={this.props.data} title={"My Salvoes Fired"} gridType={"sa"}
-                  takenPositions={this.props.mySalvoesArray} newTakenPositions={[]}/>
+            <div className="grids-results">
+                <Grid data={this.props.data} title={"My Ships"} gridType={"sh"}
+                      sunkenPositions={this.props.sunkenPositions} takenPositions={this.props.allShipsArray} toBeRemoved={[]}/>
+                <Grid data={this.props.data} title={"My Salvoes Fired"} gridType={"sa"}
+                      takenPositions={this.props.mySalvoesArray} newTakenPositions={[]} toBeRemoved={[]}/>
+            </div>
             </div>
             <div className="button-and-results">
             <button onClick={this.props.onClikFireSalvoes}>Fire Next Round of Salvoes</button>
